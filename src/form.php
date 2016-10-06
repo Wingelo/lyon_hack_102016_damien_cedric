e53ab5af22150dcd8c7a244b15734278ac4e5098
dbeauseigneur <dbeauseigneur@gmail.com>
2016-10-06 20:00:40 +0200

    add the form.php

---
+++ src/form.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * User: damien
+ * Date: 22/09/16
+ * Time: 11:08
+ */
+include("bdd.php");
+
+$connection = GetConnection();
+
+if (count($_POST) == 0) {
+
+} else {
+    $lastName = $_POST['lastName'];
+    $firstName = $_POST['firstName'];
+    $urlImage = $_POST['urlImage'];
+
+    $result = ExecSql($connection,"Select 1 from chaussettes where nom = '$lastName' and prenom = '$first'");
+    if ($user = $connection->fetch_assoc()) {
+        $query = "update user set url_chaussette = '$urlImage' " ;
+        $query .= "where nom = '$lastname' and prenom = '$firstName')";
+
+    } else {
+
+        $query = "insert into user (nom,prenom,age,email) VALUES ('" . $_POST['nom'] . "', '";
+        $query .= $_POST['prenom'] . "', '" . $_POST['url_image'] . "');";
+    }
+//    ECHO $query;
+    ExecSql($connection, $query);
+}
+//    $query = "insert into user (nom, prenom, age, email) values " .$_POST() ;
+header('Location: ../public/index.php');
